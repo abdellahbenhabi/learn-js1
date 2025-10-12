d5675bea4f593b21d30572f4b0328d3a639b4fc8
abdou <benhabiabdellah@gmail.com>
2025-10-12 09:34:52 +0100

    same

---
+++ seperate_name.js
@@ -0,0 +1,7 @@
+    const email = "seyf@elfather.dz"
+
+    const result3 = document.getElementById("task3result");
+
+    const name = email.split("@")[0];
+
+    document.body.innerHTML += `<p>${name}</p>`
